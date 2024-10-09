b40cf99810f7d864f5cd04c6f924cff12a258f89
harivuguru <harivuguru@gmail.com>
2024-10-09 15:47:23 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,28 @@
-class demo{
-    public static void main(String args[])
-    {
-        System.out.println("Hello World");
+import java.io.*;
+
+// Drive Class
+class demo {
+      // Main Function
+    public static void main (String[] args) {
+          
+        // Arithmetic operators on integers
+        int a = 10;
+        int b = 3;
+      
+        // Arithmetic operators on Strings
+        String num1 = "15";
+        String num2 = "25";
+
+        // Convert Strings to integers
+        int a1 = Integer.parseInt(num1);
+        int b1 = Integer.parseInt(num2);
+           
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("a1 + b1 = " + (a1 + b1)); 
+          
     }
 }
